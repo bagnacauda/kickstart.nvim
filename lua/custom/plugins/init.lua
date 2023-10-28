2a09667b79b26fde539365d69e7874f90281fb8a
Simone Dotto <simonedotto@protonmail.com>
2023-10-28 11:58:30 +0200

    catpuccin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- Theme inspired by Atom
+	'catppuccin/nvim',
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({ transparent_background = true, flavour = "macchiato"})
+		vim.cmd.colorscheme 'catppuccin'
+	end,
+}
